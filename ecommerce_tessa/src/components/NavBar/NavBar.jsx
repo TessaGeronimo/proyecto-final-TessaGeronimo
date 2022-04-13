5dcc54910cf2e08e8a0b5a1fa2252cdf53fa0e96
GTessa <gtessauade@gmail.com>
2022-04-12 21:07:04 -0300

    avances navBar

---
+++ ecommerce_tessa/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css'
+
+function NavBar(props) {
+    return (
+        <div >
+            <ul className='nav'>
+                <li>Marca</li>
+                <li>Categorias</li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
