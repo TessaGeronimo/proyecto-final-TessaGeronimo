3e277ca96b4fc4dbe7e57271e7f02e91a6dbe4b4
TessaGeronimo <tessa.geronimoe@gmail.com>
2022-04-28 23:14:01 -0300

    Actividad 5 finalizada

--- ecommerce_tessa/src/components/Item/Item.jsx
+++ ecommerce_tessa/src/components/Item/Item.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './Item.css';
 
 const Item = (props) => {
-    
+    console.log(props)
     return (
         <div className="card">
-            <h2>{props.nombre}</h2>
-            
-            <button id='add' >Agregar al carrito</button>
+            <h2>{props.vuelo.destino}</h2>
+            <p>{props.vuelo.precio}</p>
+            <div className='img-container'>
+                <img src= {props.vuelo.imageUrl} alt={props.vuelo.destino} ></img>
+            </div>
+            <button id='add' >Detalle</button>
         </div>
     );
 };
--- ecommerce_tessa/src/components/ItemList/ItemList.jsx
+++ ecommerce_tessa/src/components/ItemList/ItemList.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Item from '../Item/Item';
+import './ItemList.css';
 
-const prueba = [
-    {
-    id:1,
-    nombre:'Geronimo',
-    apellido: 'tessa'
-    },
-    {
-    id:2,
-    nombre:'buji',
-    apellido: 'tessa'
-    },
-    {
-    id:3,
-    nombre:'muna',
-    apellido: 'tessa'
-    }
-]
 
+const getVuelos = () => {
+    const myPromise = new Promise((resolve, reject) => {
+        const vuelosList = [
+            {
+                id:1,
+                destino:'Barcelona',
+                precio: '$90000',
+                imageUrl:'https://images.adsttc.com/media/images/5ff4/88a7/63c0/17cd/f900/0527/medium_jpg/shutterstock_397537417.jpg?1609861261'
+            },
+            {
+                id:2,
+                destino:'Paris',
+                precio: '$70000',
+                imageUrl:'https://i0.wp.com/hipertextual.com/wp-content/uploads/2013/04/Paris.jpg?w=1024&quality=50&strip=all&ssl=1'
+            },
+            {
+                id:3,
+                destino:'Londres',
+                precio: '$80000',
+                imageUrl:'https://resizer.glanacion.com/resizer/Ug6L5UfHTETFMxSKy6P5hfP5m58=/1920x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/HGNP4GG2WFAZHKAI2NU4Q54W5Y.jpg'
+            },
+        ];
+      setTimeout(() => {
+        resolve(vuelosList);
+      }, 2000);
+    });
+    return myPromise;
+  }
 
 const ItemList = () => {
-    return(
-        <div>
-        {prueba.map(p =>  <Item nombre = {p.nombre}></Item>) }
 
+    const [vuelos, setVuelos] = useState([]);
+
+    useEffect(() => {
+        getVuelos()
+        .then(res => {
+            setVuelos(res);
+        })
+    }, []);
+
+    return(
+        <div className='list-item'>
+            {vuelos.map((vuelo, i) => <Item vuelo={vuelo} key={vuelo.id} />)}
         </div>
     );
 };
