6d6cc7f0f53a76e7dd49d112ae18cec8be1459f3
TessaGeronimo <tessa.geronimoe@gmail.com>
2022-05-04 16:50:36 -0300

    branch de pruebas

--- ecommerce_tessa/src/App.jsx
+++ ecommerce_tessa/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-// import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+//import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <header className="App-header">
-       {/* <ItemListContainer greetings="Bienvenido a CoderViajes"/> */}
-        <ItemDetailContainer></ItemDetailContainer>
+        <ItemListContainer greetings="Bienvenido a CoderViajes"/> 
+        {/* <ItemDetailContainer></ItemDetailContainer> */}
       </header>
       
     </div>
