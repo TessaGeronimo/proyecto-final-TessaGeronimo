3e8cb1def0a7589cbac5513cfb61043d226840fd
TessaGeronimo <tessa.geronimoe@gmail.com>
2022-06-07 17:03:55 -0300

    avances css

--- ecommerce_tessa/src/components/Item/Item.jsx
+++ ecommerce_tessa/src/components/Item/Item.jsx
@@ -3,14 +3,14 @@ import './Item.css';
 import {Link} from 'react-router-dom';
 
 const Item = (props) => {
-
     return (
         <div className='card'>
-            <h2>{props.vuelo.destino}</h2>
-            <p>{props.vuelo.precio}</p>
             <div className='img-container'>
                 <img src= {props.vuelo.imageUrl} alt={props.vuelo.destino} ></img>
             </div>
+            <h2>{props.vuelo.destino}</h2>
+            <p>{props.vuelo.precio}</p>
+            
             <Link id='detalle' to= {'/detalle/'+props.vuelo.id}>Detalle</Link>
         </div>
     );
